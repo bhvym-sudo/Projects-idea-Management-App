21d20d1a9cd812b6bd7011317e34780012fba8bb
Bhvym <96626406+bhvym-sudo@users.noreply.github.com>
2025-03-06 17:59:19 +0530

    corrected bruh

--- main.py
+++ main.py
@@ -34,4 +34,4 @@ def main():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(host="192.168.1.4",debug=True, port=80)
+    app.run(host="0.0.0.0",debug=True, port=80)
